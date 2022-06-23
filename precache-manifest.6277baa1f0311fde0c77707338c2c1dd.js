11193ccd4bff343d84a2a48a8cbf3d4daf38352e
emesic23 <esmirmesic23@gmail.com>
2022-06-22 21:25:14 -0700

    Updates

---
+++ precache-manifest.6277baa1f0311fde0c77707338c2c1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff92a37f6720c0567215f60b597ca929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e85a2e24843602df9b",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "00b2280f48a95a85a38d",
+    "url": "/static/css/main.095cb673.chunk.css"
+  },
+  {
+    "revision": "b1e85a2e24843602df9b",
+    "url": "/static/js/2.9620832f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.9620832f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b2280f48a95a85a38d",
+    "url": "/static/js/main.1a85c6ef.chunk.js"
+  },
+  {
+    "revision": "7dcd9e7d919d94a2ac30",
+    "url": "/static/js/runtime-main.be8ba54a.js"
+  }
+]);
